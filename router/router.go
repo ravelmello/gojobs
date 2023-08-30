0d46180fae27f51b3895e8774389d0440e568e00
ravelmello <ravelmello3@gmail.com>
2023-08-30 10:04:22 -0300

    feat: add structure logic of routes

--- router/router.go
+++ router/router.go
@@ -4,11 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func InitializeRoutes() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	initializeRoutes(router)
+
 	err := router.Run(":9090")
 	if err != nil {
 		panic("Error in construction router")
