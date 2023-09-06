fb023ca6abd1867e07d4d58ff046cbf3c660c4f9
ravelmello <ravelmello3@gmail.com>
2023-09-06 14:14:53 -0300

    feat: add logic of create a resource api

--- handler/jobs-opportunities/jobCreateOpportunity.go
+++ handler/jobs-opportunities/jobCreateOpportunity.go
@@ -2,11 +2,52 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ravelmello/gojobs/config"
+	"github.com/ravelmello/gojobs/schemas"
+	"gorm.io/gorm"
 	"net/http"
 )
 
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
 func CreateJobHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"Message": "Open jobs-opportunities",
-	})
+	request := Jobs{}
+	logger = config.GetLogger("Create Job Handler")
+	db = config.GetDatabase()
+
+	logger.InfoF("Starting")
+
+	errorBuild := context.BindJSON(&request)
+	if errorBuild != nil {
+		return
+	}
+
+	err := request.Validate()
+	if err != nil {
+		logger.ErrorF("Error in request %v", err.Error())
+		sendError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	logger.InfoF("Request Received %+v", request)
+
+	job := schemas.Job{
+		Role:        request.Role,
+		Remote:      *request.Remote,
+		Description: request.Description,
+		Link:        request.Link,
+		Company:     request.Company,
+		Location:    request.Location,
+		Salary:      request.Salary,
+	}
+
+	if err := db.Create(&job).Error; err != nil {
+		logger.ErrorF("Error in creation %v", err.Error())
+		sendError(context, http.StatusInternalServerError, "error in open database")
+		return
+	}
+	sendSuccess(context, http.StatusCreated, "Operation in Create Job Handler, successfully", job)
 }
--- schemas/job.go
+++ schemas/job.go
@@ -1,6 +1,9 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Job struct {
 	gorm.Model
@@ -11,4 +14,19 @@ type Job struct {
 	Remote      bool
 	Link        string
 	Salary      int64
+}
+
+type JobResponse struct {
+	Id          uint      `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	ExpiredAt   time.Time `json:"expired_at"`
+	DeletedAt   time.Time `json:"deleted_at,omitempty"`
+	Role        string    `json:"role"`
+	Company     string    `json:"company"`
+	Location    string    `json:"location"`
+	Description string    `json:"description"`
+	Remote      bool      `json:"isRemote"`
+	Link        string    `json:"link"`
+	Salary      int64     `json:"salary"`
 }
